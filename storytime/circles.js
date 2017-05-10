a81ba9aa900fcfb7a283433380bf7afd7009e941
Clint Mullins <syntheticcfm@gmail.com>
2017-05-09 22:49:06 -0700

    new stories

---
+++ storytime/circles.js
@@ -0,0 +1,8 @@
+let circles = `
+  Do you ever feel like you are moving in circles?
+  Like everything you do just leads into the next thing you'll do?
+  But the next thing you'll do is a lot like the last thing you did?
+  And it leads you to ask the same questions over and over?
+  When it never really feels like there's any end?
+  And so you ask your friend the same questions like,
+`;
---
+++ storytime/short.js
@@ -0,0 +1,3 @@
+let short = `
+  some stories are short but you like them so you read them again like
+`;
